5fa95ad7ec9a7fb3e932b2d0ecec5ed6a655697a
Shaps Benkau <shapsuk@me.com>
2020-02-20 19:00:18 +0000

    Delegate methods now return the flow layout defaults

--- Sources/ComposedLayouts/CollectionCoordinator+FlowLayout.swift
+++ Sources/ComposedLayouts/CollectionCoordinator+FlowLayout.swift
@@ -4,32 +4,44 @@ import ComposedUI
 extension CollectionCoordinator: UICollectionViewDelegateFlowLayout {
 
     public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        guard let section = sectionProvider.sections[indexPath.section] as? CollectionFlowLayoutHandler else { return .zero }
+        guard let section = sectionProvider.sections[indexPath.section] as? CollectionFlowLayoutHandler else {
+            return (collectionViewLayout as? UICollectionViewFlowLayout)?.itemSize ?? .zero
+        }
         return section.sizeForItem(at: indexPath.item, traitCollection: collectionView.traitCollection)
     }
 
     public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        guard let section = sectionProvider.sections[section] as? CollectionFlowLayoutHandler else { return .zero }
+        guard let section = sectionProvider.sections[section] as? CollectionFlowLayoutHandler else {
+            return (collectionViewLayout as? UICollectionViewFlowLayout)?.sectionInset ?? .zero
+        }
         return section.layoutMetrics(traitCollection: collectionView.traitCollection).contentInsets
     }
 
     public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        guard let section = sectionProvider.sections[section] as? CollectionFlowLayoutHandler else { return 0 }
+        guard let section = sectionProvider.sections[section] as? CollectionFlowLayoutHandler else {
+            return (collectionViewLayout as? UICollectionViewFlowLayout)?.minimumLineSpacing ?? 0
+        }
         return section.layoutMetrics(traitCollection: collectionView.traitCollection).minimumLineSpacing
     }
 
     public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        guard let section = sectionProvider.sections[section] as? CollectionFlowLayoutHandler else { return 0 }
+        guard let section = sectionProvider.sections[section] as? CollectionFlowLayoutHandler else {
+            return (collectionViewLayout as? UICollectionViewFlowLayout)?.minimumInteritemSpacing ?? 0
+        }
         return section.layoutMetrics(traitCollection: collectionView.traitCollection).minimumInteritemSpacing
     }
 
     public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-        guard let section = sectionProvider.sections[section] as? CollectionFlowLayoutHandler else { return .zero }
+        guard let section = sectionProvider.sections[section] as? CollectionFlowLayoutHandler else {
+            return (collectionViewLayout as? UICollectionViewFlowLayout)?.headerReferenceSize ?? .zero
+        }
         return section.referenceHeaderSize(traitCollection: collectionView.traitCollection)
     }
 
     public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
-        guard let section = sectionProvider.sections[section] as? CollectionFlowLayoutHandler else { return .zero }
+        guard let section = sectionProvider.sections[section] as? CollectionFlowLayoutHandler else {
+            return (collectionViewLayout as? UICollectionViewFlowLayout)?.footerReferenceSize ?? .zero
+        }
         return section.referenceFooterSize(traitCollection: collectionView.traitCollection)
     }
 
