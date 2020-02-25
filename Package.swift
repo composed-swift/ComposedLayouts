5bf7ee63f2abf9511c24cfb1d8ef6f4aebfa5421
Shaps Benkau <shapsuk@me.com>
2020-02-25 14:46:06 +0000

    Fixes an issue where ComposedLayouts wasn't correctly defining its ComposedUI dependency

--- Package.swift
+++ Package.swift
@@ -23,6 +23,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ComposedLayouts",
-            dependencies: []),
+            dependencies: ["ComposedUI"]),
     ]
 )
