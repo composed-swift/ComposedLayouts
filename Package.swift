e260a53b01e3a9d0b282cbf4b33de03f2d1eea15
Shaps Benkau <shapsuk@me.com>
2020-02-25 00:51:07 +0000

    Migrated all sections to the new AppCoordinator

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version:5.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -7,22 +6,19 @@ let package = Package(
     name: "ComposedLayouts",
     platforms: [
         .iOS(.v11)
+
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "ComposedLayouts",
             targets: ["ComposedLayouts"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/composed-swift/ComposedUI", .branch("master")),
+        .package(url: "https://github.com/composed-swift/ComposedUI", from: "0.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ComposedLayouts",
-            dependencies: []),
+            dependencies: ["ComposedUI"]),
     ]
 )
